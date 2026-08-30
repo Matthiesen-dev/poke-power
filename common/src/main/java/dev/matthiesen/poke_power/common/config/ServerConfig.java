54638c361595cb0bd73b4688dc4e327138dec5aa
Adam Matthiesen <amatthiesen@outlook.com>
2026-08-30 03:53:50 -0700

    Refactor configuration keys in ServerConfig for power block and cable settings

--- common/src/main/java/dev/matthiesen/poke_power/common/config/ServerConfig.java
+++ common/src/main/java/dev/matthiesen/poke_power/common/config/ServerConfig.java
@@ -23,19 +23,19 @@ public final class ServerConfig {
         builder.comment("Power Block Config").push("powerBlock");
         blocks_powerBlock_capacity = builder
                 .comment("The amount of power that a power block can store")
-                .defineInRange("powerBlockCapacity", 120000L, 1L, Long.MAX_VALUE);
+                .defineInRange("capacity", 120000L, 1L, Long.MAX_VALUE);
         blocks_powerMax_extract = builder
                 .comment("The maximum amount of power that can be extracted from a power block per tick")
-                .defineInRange("powerMaxExtract", 16000L, 1L, Long.MAX_VALUE);
+                .defineInRange("maxExtract", 16000L, 1L, Long.MAX_VALUE);
         builder.pop();
 
         builder.comment("Cable Config").push("cable");
         blocks_cable_capacity = builder
                 .comment("The amount of power that a cable can store")
-                .defineInRange("cableCapacity", 32000L, 1L, Long.MAX_VALUE);
+                .defineInRange("capacity", 32000L, 1L, Long.MAX_VALUE);
         blocks_cable_maxExtract = builder
                 .comment("The maximum amount of power that can be extracted from a cable per tick")
-                .defineInRange("cableMaxExtract", 16000L, 1L, Long.MAX_VALUE);
+                .defineInRange("maxExtract", 16000L, 1L, Long.MAX_VALUE);
         builder.pop();
 
         builder.pop();
