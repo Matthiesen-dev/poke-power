4e2822276a867df69791cb2796943639bbfc5dae
Adam Matthiesen <amatthiesen@outlook.com>
2026-09-01 03:53:51 -0700

    Refactor Item classes, and improve jade overlay (#16)
    * Implement AbstractGeckoItem for enhanced item animation and update PowerBlockItem to extend it; add tooltip for total energy generation in JadeProvider
    
    * Update PowerBlock tooltip to display total energy generation and add Pokémon level information
    
    * Update tooltip key for total energy generation in PowerBlock JadeProvider
    
    * Add translation for Pokémon energy generation tooltip in PowerBlock JadeProvider

--- common/src/main/java/dev/matthiesen/poke_power/common/item/PowerBlockItem.java
+++ common/src/main/java/dev/matthiesen/poke_power/common/item/PowerBlockItem.java
@@ -1,45 +1,12 @@
 package dev.matthiesen.poke_power.common.item;
 
 import dev.matthiesen.poke_power.common.registry.BlockRegistry;
-import net.minecraft.world.item.BlockItem;
-import org.apache.commons.lang3.mutable.MutableObject;
-import software.bernie.geckolib.animatable.GeoItem;
-import software.bernie.geckolib.animatable.client.GeoRenderProvider;
-import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.animatable.instance.SingletonAnimatableInstanceCache;
-import software.bernie.geckolib.animation.AnimatableManager;
-import software.bernie.geckolib.animation.AnimationController;
 import software.bernie.geckolib.animation.RawAnimation;
 
-import java.util.function.Consumer;
-
-public final class PowerBlockItem extends BlockItem implements GeoItem {
+public final class PowerBlockItem extends AbstractGeckoItem {
     private static final RawAnimation IDLE_ANIMATION = RawAnimation.begin().thenLoop("animation.power_block.idle");
-    private final AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
-    public final MutableObject<GeoRenderProvider> renderProviderHolder = new MutableObject<>();
-
-    public RawAnimation getIdleAnimation() {
-        return IDLE_ANIMATION;
-    }
 
     public PowerBlockItem() {
-        super(BlockRegistry.POWER_BLOCK.get(), new Properties());
-    }
-
-    @Override
-    public void createGeoRenderer(Consumer<GeoRenderProvider> consumer) {
-        consumer.accept(this.renderProviderHolder.getValue());
-    }
-
-    @Override
-    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        controllers.add(new AnimationController<>(this, state ->
-                state.setAndContinue(getIdleAnimation())
-        ));
-    }
-
-    @Override
-    public AnimatableInstanceCache getAnimatableInstanceCache() {
-        return this.cache;
+        super(IDLE_ANIMATION, BlockRegistry.POWER_BLOCK.get());
     }
 }
