d1c30ea8a528f7ff81a7424bf2e45e52a2002980
Adam Matthiesen <amatthiesen@outlook.com>
2026-09-20 12:42:02 -0700

    Refactor PokePowerNeoForge to accept IEventBus and update PowerToolsNeoForge to use modBus for event registration (#22)

--- neoforge/src/main/java/dev/matthiesen/poke_power/neoforge/PokePowerNeoForge.java
+++ neoforge/src/main/java/dev/matthiesen/poke_power/neoforge/PokePowerNeoForge.java
@@ -1,14 +1,17 @@
 package dev.matthiesen.poke_power.neoforge;
 
 import dev.matthiesen.poke_power.common.PokePowerCommon;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(PokePowerCommon.MOD_ID)
 public final class PokePowerNeoForge {
     public static final PokePowerCommon INSTANCE = PokePowerCommon.INSTANCE;
+    public static volatile IEventBus modBus;
 
-    public PokePowerNeoForge() {
+    public PokePowerNeoForge(IEventBus modBus) {
         INSTANCE.createInfoLog("Loading for NeoForge Mod Loader");
+        PokePowerNeoForge.modBus = modBus;
         INSTANCE.initialize();
     }
 }
