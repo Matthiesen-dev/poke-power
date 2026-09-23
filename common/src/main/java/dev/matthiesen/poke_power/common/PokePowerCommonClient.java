b7d16cfe96b042ed8df8f9f03f1f518ec9e1d55c
Adam Matthiesen <amatthiesen@outlook.com>
2026-09-22 18:09:42 -0700

    Register: relocate S2C packet handlers to register in the server-side mod (#24)

--- common/src/main/java/dev/matthiesen/poke_power/common/PokePowerCommonClient.java
+++ common/src/main/java/dev/matthiesen/poke_power/common/PokePowerCommonClient.java
@@ -3,7 +3,6 @@ package dev.matthiesen.poke_power.common;
 import dev.matthiesen.matthiesen_core.common.AbstractCommonClientMod;
 import dev.matthiesen.poke_power.common.client.renderer.PowerBlockEntityRenderer;
 import dev.matthiesen.poke_power.common.client.screen.PowerBlockScreen;
-import dev.matthiesen.poke_power.common.network.SyncGeneratorPayload;
 import dev.matthiesen.poke_power.common.registry.BlockEntityRegistry;
 import dev.matthiesen.poke_power.common.registry.MenuRegistry;
 
@@ -17,10 +16,6 @@ public final class PokePowerCommonClient extends AbstractCommonClientMod {
     @Override
     public void initialize() {
         createInfoLog("Initializing client");
-
-        // Register the client-bound (S2C) sync packet handler
-        PokePowerCommon.INSTANCE.getNetworkingManager()
-                .registerS2C(SyncGeneratorPayload.TYPE, SyncGeneratorPayload.CODEC, SyncGeneratorPayload::handleClient);
     }
 
     public void registerRenderers() {
